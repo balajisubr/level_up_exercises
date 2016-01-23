8928ac81ba08be0eaf64dd07773f8f5cb1e8cdc0
Balaji Subramanian <bsubramanian@enova.com>
2016-01-23 15:58:02 -0600

    Initial pmail changes

---
+++ pmail/email.js
@@ -0,0 +1,8 @@
+$('document').ready(function() {
+ $('#submit').on('click', function() {
+  location.href="/Users/bsubramanian/level_up_exercises/pmail/email_list.html";
+});
+$('.my_account').on('click', function() {
+    $('.more_details').toggle(); 
+});
+});
